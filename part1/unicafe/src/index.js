028a14cdc101bafbb58a792104d3b0655e551c48
Galen Helgemo <galen@helgemo.us>
2021-02-23 22:20:15 -0600

    part1 Exercise 1.8 unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,6 +7,18 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => (
+  <>
+    <h1>statistics</h1>
+      good {good}<br/>
+      neutral {neutral}<br/>
+      bad {bad}<br/>
+      all {good + neutral + bad}<br/>
+      average {(good * 1 + bad * -1)/(good + neutral + bad)}<br/>
+      positive {(good / (good + neutral + bad)) * 100} %
+  </>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -19,13 +31,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="bad"/>
-      <h1>statistics</h1>
-      good {good}<br/>
-      neutral {neutral}<br/>
-      bad {bad}<br/>
-      all {good + neutral + bad}<br/>
-      average {(good * 1 + bad * -1)/(good + neutral + bad)}<br/>
-      positive {(good / (good + neutral + bad)) * 100} %
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
