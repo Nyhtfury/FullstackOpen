2513e2eb066c9f358c36720d6798bf580b56adbf
Galen Helgemo <galen@helgemo.us>
2021-03-02 21:34:17 -0600

    part1 Exercise 1.13 anecdotes step2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,37 +1,67 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 const Button = ({handleClick, text}) => (
     <button onClick={handleClick}>
-      {text}
+        {text}
     </button>
 )
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
+const Indicator = ({verb, value, unit}) => (
+    <>
+        {verb} {value} {unit}
+    </>
+)
 
-  return (
-    <div>
-      {props.anecdotes[selected]}<br/>
-      <Button text={"Next Anecdote"} handleClick={() => setSelected(getRandomInt(props.anecdotes.length))}/>
-    </div>
-  )
+const VoteCounter = ({value}) => {
+    if (value === 1)
+        return (
+            <>
+                <Indicator verb={"Has"} value={value} unit={"vote"}/>
+            </>
+        )
+
+    return (
+        <>
+            <Indicator verb={"Has"} value={value} unit={"votes"}/>
+        </>
+    )
+}
+
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(Array.apply(null, new Array(props.anecdotes.length)).map(Number.prototype.valueOf, 0))
+
+    const voteForAnecdote = position => {
+        let newVotes = {...votes}
+        newVotes[position] += 1
+        setVotes(newVotes)
+    }
+
+    return (
+        <div>
+            {props.anecdotes[selected]}<br/>
+            <VoteCounter value={votes[selected]}/><br/>
+            <Button text={"vote"} handleClick={() => voteForAnecdote(selected)}/>
+            <Button text={"Next Anecdote"} handleClick={() => setSelected(getRandomInt(props.anecdotes.length))}/>
+        </div>
+    )
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
+    <App anecdotes={anecdotes}/>,
+    document.getElementById('root')
 )
