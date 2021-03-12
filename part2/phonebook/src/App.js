d155897e60d8934f826f8ba551a30599473bb5c7
Galen Helgemo <galen@helgemo.us>
2021-03-12 11:26:14 -0600

    part2 Exercise 2.8 phonebook step3 - filter state doesn't align with displayed persons.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,45 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
+    const [ filteredPersons, setFilteredPersons ] = useState(persons)
     const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
+    const [ filter, setFilter ] = useState('')
 
     const handleNewName = (event) => {
         setNewName(event.target.value)
     }
 
-    const addName = (event) => {
+    const handleNewNumber = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const handleFilter = (event) => {
+        setFilter(event.target.value)
+        setFilteredPersons(persons.filter(person => Object.values(person)
+            .map(el => el.includes(filter))
+            .includes(true)))
+    }
+
+    const addPerson = (event) => {
         event.preventDefault()
+
+        if (!newName) {
+            alert("Please enter a name before proceeding.")
+            return;
+        } else if (!newNumber) {
+            alert("Please enter a number before proceeding.")
+            return;
+        }
+
         const newPerson = {
-            name: newName
+            name: newName,
+            number: newNumber
         }
 
         let duplicate = false;
@@ -30,27 +57,37 @@ const App = () => {
             setPersons(persons.concat(newPerson))
             setNewName('')
         }
-
     }
 
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+                <div>
+                    <label>filter shown with</label>
+                    <input value={filter} onChange={handleFilter}/>
+                </div>
+            <h2>add a new</h2>
             <form>
                 <div>
-                    name: <input value={newName} onChange={handleNewName}/>
+                    <label>name:</label>
+                    <input value={newName} onChange={handleNewName}/>
                 </div>
                 <div>
-                    <button type="submit" onClick={addName}>add</button>
+                    <label>number:</label>
+                    <input value={newNumber} onChange={handleNewNumber}/>
+                </div>
+                <div>
+                    <button type="submit" onClick={addPerson}>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             <ul>
-                {persons.map(person => (
-                    <li key={person.name}>
-                        {person.name}
-                    </li>
-                ))}
+                {filteredPersons.map(person => (
+                        <li key={person.name}>
+                            {person.name} {person.number}
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
